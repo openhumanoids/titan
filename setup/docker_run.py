8fdcf8d37a3c1714dbdcd4ea8c1ed13b8278c85b
Robin Deits <robin.deits@gmail.com>
2018-01-22 15:57:12 -0500

    more docker adventures

--- setup/docker_run.py
+++ setup/docker_run.py
@@ -15,10 +15,13 @@ if __name__ == '__main__':
     docker run \
       --name titan \
       -v {source_dir}:/titan \
+      -v {bazel_cache}:{bazel_cache} \
       -it \
       --rm \
       titan-dependencies \
       {command}\
-    """.format(source_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."), command=command)
+    """.format(source_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."),
+               command=command,
+               bazel_cache=os.path.join(os.environ["HOME"], ".cache", "bazel"))
     print("command:", cmd)
     os.system(cmd)
--- setup/titan.dockerfile
+++ setup/titan.dockerfile
@@ -5,3 +5,13 @@ COPY ./install_drake_prereqs.sh /tmp/install_drake_prereqs.sh
 RUN apt-get update && \
     yes "Y" | /tmp/install_titan_prereqs.sh && \
     yes "Y" | /tmp/install_drake_prereqs.sh
+
+ARG USER_NAME
+ARG USER_ID
+ARG USER_GID
+
+RUN useradd -ms /bin/bash $USER_NAME
+RUN usermod -u $USER_ID $USER_NAME
+RUN groupmod -g $USER_GID $USER_NAME
+
+USER $USER_NAME
